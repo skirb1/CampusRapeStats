1fc1c7e3880f8a69047e1c69b4cdb0058e023486
Sarah Kirby <skirb1@umbc.edu>
2016-10-02 04:58:09 -0400

    notfound complete
    complete list of schools not found with python geolocator in
    notfound.txt

--- geocode_schools.py
+++ geocode_schools.py
@@ -9,25 +9,26 @@ db = client.campus_crime
 geolocator = Nominatim()
 
 #iterate through campuses
-cursor = db.campuses.find()
+cursor = db.campuses.find({ "lat": 0, "long": 0}).skip(200)
 
 for record in cursor :
-	if record["lat"] == 0 and record["long"] == 0 :
-		school = record["school_name"]
-		campus = record["campus_name"]
+	school = record["school_name"]
+	campus = record["campus_name"]
 
-		full_name = school + " " + campus
-		location = geolocator.geocode(full_name, exactly_one = True)
-		if(location != None) :
-			print(full_name)
-			
-			result = db.campuses.update(
-				{"school_name" : school, "campus_name": campus},
-				{"$set" : { "lat" : location.latitude, "long" : location.longitude }}
-				)
+	'''
+	full_name = school + " " + campus
+	location = geolocator.geocode(full_name, exactly_one = True)
+	if(location != None) :
+		print(full_name)
+		
+		result = db.campuses.update(
+			{"school_name" : school, "campus_name": campus},
+			{"$set" : { "lat" : location.latitude, "long" : location.longitude }}
+			)
 
-		else :
-			file.write(school + "," + campus + "\n")
+	else :
+	'''
+	file.write(school + "," + campus + "\n")
 
 
 file.close()
